dbe3b91c07d280721c1dd897a9db68c46b56685f
DPatel1416 <dhruvkumarpatel07@gmail.com>
2024-02-06 16:57:30 -0500

    server side added

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -1,38 +1,91 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import { preview } from "../assets";
-import { getRandomPrompt } from "../utils";
-import { Loader, FormField } from "../components";
+import { preview } from '../assets';
+import { getRandomPrompt } from '../utils';
+import { FormField, Loader } from '../components';
+
 const CreatePost = () => {
   const navigate = useNavigate();
+
   const [form, setForm] = useState({
-    name: "",
-    prompt: "",
-    photo: "",
+    name: '',
+    prompt: '',
+    photo: '',
   });
+
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
-  const generateImage = () => {};
-  const handleSubmit = () => {};
-  const handleChange = (e) => {};
-  const handleSurpriseMe = () => {};
+
+  const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
+
+  const handleSurpriseMe = () => {
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt });
+  };
+
+  const generateImage = async () => {
+    if (form.prompt) {
+      try {
+        setGeneratingImg(true);
+        const response = await fetch('https://dalle-arbb.onrender.com/api/v1/dalle', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            prompt: form.prompt,
+          }),
+        });
+
+        const data = await response.json();
+        setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
+      } catch (err) {
+        alert(err);
+      } finally {
+        setGeneratingImg(false);
+      }
+    } else {
+      alert('Please provide proper prompt');
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (form.prompt && form.photo) {
+      setLoading(true);
+      try {
+        const response = await fetch('https://dalle-arbb.onrender.com/api/v1/post', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ ...form }),
+        });
+
+        await response.json();
+        alert('Success');
+        navigate('/');
+      } catch (err) {
+        alert(err);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      alert('Please generate an image with proper details');
+    }
+  };
 
   return (
     <section className="max-w-7xl mx-auto">
-      {/* Section for title and description */}
       <div>
         <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
-        <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">
-          Generate an imaginative image through DALL-E AI and share it with the
-          community
-        </p>
+        <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">Generate an imaginative image through DALL-E AI and share it with the community</p>
       </div>
 
-      {/* Form for creating a post */}
       <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
         <div className="flex flex-col gap-5">
-          {/* Form field for name */}
           <FormField
             labelName="Your Name"
             type="text"
@@ -42,7 +95,6 @@ const CreatePost = () => {
             handleChange={handleChange}
           />
 
-          {/* Form field for prompt */}
           <FormField
             labelName="Prompt"
             type="text"
@@ -54,11 +106,8 @@ const CreatePost = () => {
             handleSurpriseMe={handleSurpriseMe}
           />
 
-          {/* Container for displaying generated image */}
-          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm 
-                  ounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center 
-                  items-center">
-            {form.photo ? (
+          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+            { form.photo ? (
               <img
                 src={form.photo}
                 alt={form.prompt}
@@ -72,43 +121,31 @@ const CreatePost = () => {
               />
             )}
 
-            {/* Loader component for image generation loading */}
             {generatingImg && (
-              <div
-                className="absolute inset-0 z-0 flex justify-center 
-              items-center bg-[rgba(0,0,0,0.5)] rounded-lg"
-              >
+              <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
                 <Loader />
               </div>
             )}
           </div>
         </div>
 
-        {/* Button for generating image */}
         <div className="mt-5 flex gap-5">
           <button
             type="button"
             onClick={generateImage}
-            className=" text-white bg-green-700 font-medium 
-            rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+            className=" text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
           >
-            {generatingImg ? "Generating..." : "Generate"}
+            {generatingImg ? 'Generating...' : 'Generate'}
           </button>
         </div>
 
-        {/* Section for sharing post */}
         <div className="mt-10">
-          <p className="mt-2 text-[#666e75] text-[14px]">
-            {" "}
-            Once you have created the image you want, you can share it with
-            others in the community{" "}
-          </p>
+          <p className="mt-2 text-[#666e75] text-[14px]">** Once you have created the image you want, you can share it with others in the community **</p>
           <button
             type="submit"
-            className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full 
-            sm:w-auto px-5 py-2.5 text-center"
+            className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
           >
-            {loading ? "Sharing..." : "Share with the Community"}
+            {loading ? 'Sharing...' : 'Share with the Community'}
           </button>
         </div>
       </form>
@@ -116,5 +153,4 @@ const CreatePost = () => {
   );
 };
 
-// Export the CreatePost component as the default export
 export default CreatePost;
